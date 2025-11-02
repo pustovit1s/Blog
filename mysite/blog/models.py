d775f121d7279c3e53c257b3c18063c26fafbbbe
qvbnd <pustovit.is@mail.ru>
2025-11-03 00:41:34 +0300

    разработка представлений на основе функций

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -17,7 +18,8 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                             on_delete=models.CASCADE,
                                             related_name='blog_posts')
@@ -33,7 +35,6 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-    
     class Meta:
         ordering = ['-publish']
         indexes = [
@@ -42,3 +43,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
